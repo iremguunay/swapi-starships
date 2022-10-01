07cc612aa8c5e58e080e2290259d512a194ac42e
Irem Gunay <iremguunay@gmail.com>
2022-10-01 17:55:17 +0300

    Create Detail page and add it to the route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Routes, Route} from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 // components
 import Navigation from "./components/navbar/Navigation";
@@ -15,6 +16,7 @@ function App() {
       <Navigation logo={logo} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/starships/:starshipId" element={<Detail />} />
       </Routes>
     </div>
   );
