3127872758c2be82fffa515dfd0d37fc35e00844
Irem Gunay <iremguunay@gmail.com>
2022-10-02 00:23:15 +0300

    Use detail card in the detail page

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -3,7 +3,15 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import DetailCard from "../../components/detailcard/DetailCard";
+import DetailCardImage from "../../components/detailcard/DetailCardImage";
+import DetailCardBody from "../../components/detailcard/DetailCardBody";
+import DetailCardTitle from "../../components/detailcard/DetailCardTitle";
+import DetailCardText from "../../components/detailcard/DetailCardText";
+
 import Loader from "../../components/loader/Loader";
+
+import starshipImages from "../../starshipImages.json";
 
 const Detail = () => {
   const [starship, setStarship] = useState(null);
@@ -19,10 +27,37 @@ const Detail = () => {
       .finally(() => setLoading(false));
   }, [starshipId]);
 
+  const shipImage = starshipImages.find(
+    (image) => image.name === starship?.name
+  );
+
   return (
     <>
       {loading && <Loader />}
-      <h1>Detail</h1>
+      {starship && (
+        <DetailCard>
+          <DetailCardImage src={`../${shipImage.image}`} alt={shipImage.name} />
+          <DetailCardBody>
+            <DetailCardTitle tag="FEATURES" title={starship.name} />
+            <DetailCardText>
+              <strong>Model:</strong> {starship.model}
+              <br />
+              <strong>Hyperdrive Rating:</strong> {starship.hyperdrive_rating}
+              <br />
+              <strong>Passengers:</strong> {starship.passengers}
+              <br />
+              <strong>Max Atmosphering Speed:</strong>{" "}
+              {starship.max_atmosphering_speed}
+              <br />
+              <strong>Manufacturer:</strong> {starship.manufacturer}
+              <br />
+              <strong>Crew:</strong> {starship.crew}
+              <br />
+              <strong>Cargo Capacity:</strong> {starship.cargo_capacity}
+            </DetailCardText>
+          </DetailCardBody>
+        </DetailCard>
+      )}
     </>
   );
 };
