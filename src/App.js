83393e0071a8247a721856113cde93aebdd209c5
Irem Gunay <iremguunay@gmail.com>
2022-10-01 14:31:28 +0300

    Add react-router-dom for handling navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import {Routes, Route} from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+
+// components
 import Navigation from "./components/navbar/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Navigation logo={logo} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
