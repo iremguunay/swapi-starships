5c365b02ef82a6f315714fa4dd9c83c79e526b5a
Irem Gunay <iremguunay@gmail.com>
2022-10-01 18:22:31 +0300

    Show loader when getting details from API

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -3,8 +3,11 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import Loader from "../../components/loader/Loader";
+
 const Detail = () => {
   const [starship, setStarship] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const { starshipId } = useParams();
 
@@ -12,13 +15,15 @@ const Detail = () => {
   useEffect(() => {
     axios(`${process.env.REACT_APP_API_BASE_URL}/starships/${starshipId}`)
       .then((response) => response.data)
-      .then((data) => setStarship(data));
+      .then((data) => setStarship(data))
+      .finally(() => setLoading(false));
   }, [starshipId]);
 
   return (
-    <div>
+    <>
+      {loading && <Loader />}
       <h1>Detail</h1>
-    </div>
+    </>
   );
 };
 
