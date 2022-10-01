a8dcd9d35efc4b88e929841a9430353ad436d07a
Irem Gunay <iremguunay@gmail.com>
2022-10-01 17:47:17 +0300

    Show error message if fetch rejected

---
+++ src/components/error/Error.js
@@ -0,0 +1,7 @@
+import "./error.css";
+
+const Error = (props) => {
+  return <div className="error">{props.children}</div>;
+};
+
+export default Error;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,10 @@ import "./style.css";
 import { useEffect } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { fetchStarships, getSearchResult } from "../../redux/starships/starshipsSlice";
+import {
+  fetchStarships,
+  getSearchResult,
+} from "../../redux/starships/starshipsSlice";
 
 // components
 import Card from "../../components/card/Card";
@@ -14,6 +17,7 @@ import CardTitle from "../../components/card/CardTitle";
 import LoadMore from "../../components/button/LoadMore";
 
 import Loader from "../../components/loader/Loader";
+import Error from "../../components/error/Error";
 
 import SearchBar from "../../components/search/SearchBar";
 
@@ -26,6 +30,7 @@ const Home = () => {
   const status = useSelector((state) => state.starships.status);
   const hasNextPage = useSelector((state) => state.starships.hasNextPage);
   const nextPageURL = useSelector((state) => state.starships.nextURL);
+  const error = useSelector((state) => state.starships.error);
 
   const dispatch = useDispatch();
 
@@ -41,12 +46,24 @@ const Home = () => {
 
   const handleSearch = (event) => {
     dispatch(getSearchResult(event.target.value));
+  };
+
+  if (status === "failed") {
+    return (
+      <>
+        <Error>
+          <h2>{error.code}</h2>
+          <h4>{error.message}</h4>
+          <p>It looks like one of the developers fell asleep :)</p>
+        </Error>
+      </>
+    );
   }
 
   return (
     <div className="Home">
       {/* searchBar component - start */}
-      <SearchBar placeholder="Search for..." onChange={handleSearch}/>
+      <SearchBar placeholder="Search for..." onChange={handleSearch} />
       {/* card component - start */}
       <div className="container">
         {starships.map((starship, index) => (
